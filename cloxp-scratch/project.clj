6314a2dbfd677d3a5dcd408ba43072f01b915ec7
Robert Krahn <robert.krahn@gmail.com>
2015-04-19 22:20:29 -0700

    cloxp-scratch dir

---
+++ cloxp-scratch/project.clj
@@ -0,0 +1,4 @@
+(defproject cloxp-scratch "0.1.0-SNAPSHOT"
+  :description "Just the cloxp scratch project. You can try stuff out here but your real projects should got into their own folders."
+  :url "http://cloxp.githu.io"
+  :dependencies [[org.clojure/clojure "1.6.0"]])
